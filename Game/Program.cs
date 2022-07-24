b932b8df94d349eb8e28ba535c041c75c084a105
RZFury <48477044+rzfury@users.noreply.github.com>
2022-07-24 20:07:07 +0700

    Initial Commit

---
+++ Game/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RZFNA.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Engine.Game game = new())
+            {
+                game.Window.Title = "RZFNA";
+                game.Run();
+            }
+        }
+    }
+}
